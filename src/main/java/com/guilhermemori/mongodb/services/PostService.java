6662b905f2b9696cc5f9e1aa5b9cbcf466ee5835
GuilhermeMori <guilhermebmori@hotmail.com>
2022-10-24 14:36:58 +0100

    Obtendo um post por id

---
+++ src/main/java/com/guilhermemori/mongodb/services/PostService.java
@@ -0,0 +1,22 @@
+package com.guilhermemori.mongodb.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.guilhermemori.mongodb.domain.Post;
+import com.guilhermemori.mongodb.repository.PostRepository;
+import com.guilhermemori.mongodb.services.execption.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
